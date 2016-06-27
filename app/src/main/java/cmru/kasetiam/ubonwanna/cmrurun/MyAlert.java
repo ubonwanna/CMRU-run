8d73109a5cf3a29697cac930049bd1359a2975c7
ubonwanna <ajaomcmru@gmail.com>
2016-06-27 16:36:13 +0700

    Design UI SignUp N

---
+++ app/src/main/java/cmru/kasetiam/ubonwanna/cmrurun/MyAlert.java
@@ -0,0 +1,19 @@
+package cmru.kasetiam.ubonwanna.cmrurun;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+import androi.content.context;
+
+/**
+ * Created by User on 27/6/2559.
+ */
+public class MyAlert {
+
+        public void myDialog(Context context,
+                             String strTitle,
+                             String strMessage){
+            AlertDialog.Builder builder = new AlertDialog.Builder();
+            builder.setCancelable(false);
+        }
+} // Main Class
--- app/src/main/java/cmru/kasetiam/ubonwanna/cmrurun/SignUpActivity.java
+++ app/src/main/java/cmru/kasetiam/ubonwanna/cmrurun/SignUpActivity.java
@@ -4,13 +4,37 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, passwordEditText, userEditText;
+    private RadioGroup radioGroup;
+    private RadioButton avata0RadioButton, avata1RadioButton,
+            avata2RadioButton, avata3RadioButton, avata4RadioButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+        radioGroup = (RadioGroup) findViewById(R.id.radAvata);
+        avata0RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        avata1RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        avata2RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        avata3RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+        avata4RadioButton = (RadioButton) findViewById(R.id.radioButton5);
+
+
+
     } // Main Method
 
 } // Main class
