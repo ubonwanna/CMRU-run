358c95fa023b166fde986833edb3336be449294f
ubonwanna <ajaomcmru@gmail.com>
2016-06-28 09:55:01 +0700

    check space finish

--- app/src/main/java/cmru/kasetiam/ubonwanna/cmrurun/SignUpActivity.java
+++ app/src/main/java/cmru/kasetiam/ubonwanna/cmrurun/SignUpActivity.java
@@ -15,7 +15,7 @@ public class SignUpActivity extends AppCompatActivity {
     private RadioGroup radioGroup;
     private RadioButton avata0RadioButton, avata1RadioButton,
             avata2RadioButton, avata3RadioButton, avata4RadioButton;
-
+    private String nameString, userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +35,23 @@ public class SignUpActivity extends AppCompatActivity {
 
 
 
-
     } // Main Method
+
+    public void clickSignUpSign(View view){
+
+        // Get Value From Edit Text
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().toString();
+
+        // Check Space
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
+            // Have Space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่อง ค่ะ");
+        }
+
+    }// clickSign
+
 
 } // Main class
