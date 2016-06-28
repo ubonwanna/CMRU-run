b08b2ef42b7bf56d5b36ca0790901f0b76549a9f
ubonwanna <ajaomcmru@gmail.com>
2016-06-28 11:30:13 +0700

    confirm data OK

---
+++ app/src/main/java/cmru/kasetiam/ubonwanna/cmrurun/MyData.java
@@ -0,0 +1,15 @@
+package cmru.kasetiam.ubonwanna.cmrurun;
+
+/**
+ * Created by User on 28/6/2559.
+ */
+public class MyData {
+    //Explicit
+    private int[] avataInts = new int[]{R.drawable.bird48,
+            R.drawable.doremon48, R.drawable.kon48,
+            R.drawable.nobita48, R.drawable.rat48};
+
+    public int[] getAvataInts() {
+        return avataInts;
+    }
+}
--- app/src/main/java/cmru/kasetiam/ubonwanna/cmrurun/SignUpActivity.java
+++ app/src/main/java/cmru/kasetiam/ubonwanna/cmrurun/SignUpActivity.java
@@ -1,5 +1,7 @@
 package cmru.kasetiam.ubonwanna.cmrurun;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,7 +17,7 @@ public class SignUpActivity extends AppCompatActivity {
     private RadioGroup radioGroup;
     private RadioButton avata0RadioButton, avata1RadioButton,
             avata2RadioButton, avata3RadioButton, avata4RadioButton;
-    private String nameString, userString, passwordString;
+    private String nameString, userString, passwordString, avataString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +35,31 @@ public class SignUpActivity extends AppCompatActivity {
         avata3RadioButton = (RadioButton) findViewById(R.id.radioButton4);
         avata4RadioButton = (RadioButton) findViewById(R.id.radioButton5);
 
+        //Radio Controller
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch () {
+                    case R.id.radioButton:
+
+                        avataString = "0";
+                        break;
+                    case R.id.radioButton2:
+                        avataString = "1";
+                        break;
+                    case R.id.radioButton3:
+                        avataString = "2";
+                        break;
+                    case R.id.radioButton4:
+                        avataString = "3";
+                        break;
+                    case R.id.radioButton5:
+                        avataString = "4";
+                        break;
+                } //switch
+
+            }   // onChecked
+        });
 
 
     } // Main Method
@@ -49,9 +76,65 @@ public class SignUpActivity extends AppCompatActivity {
             // Have Space
             MyAlert myAlert = new MyAlert();
             myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่อง ค่ะ");
+        } else if (checkChooseAvata ()) {
+                //checked
+
+                confirmData ();
+
+
+        } else {
+            // Not Checked
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "ยังไม่เลือก Avata", "กรุณาเลือก Avata ด้วยค่ะ");
         }
 
     }// clickSign
+
+    private void confirmData() {
+        MyData myData = new MyData();
+        int[] avataInts = myData.getAvataInts();
+
+        AlertDialog.Builder builder = new  AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setIcon(avataInts[Integer.parseInt(avataString)]);
+        builder.setTitle(nameString);
+        builder.setMessage("User =" + userString + "\n" + "Password = " + passwordString);
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                upLoadUserToServer();
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    } //confirmData
+
+    private void upLoadUserToServer() {
+
+    }
+
+    private boolean checkChooseAvata() {
+
+        boolean status = true;
+        if (avata0RadioButton.isChecked() ||
+                avata1RadioButton.isChecked() ||
+                avata2RadioButton.isChecked() ||
+                avata3RadioButton.isChecked() ||
+                avata4RadioButton.isChecked()) {
+            //Have Checked
+                status = true;
+        } else {
+            // Not Checked
+                status = false;
+        }
+        return status;
+    }
 
 
 } // Main class
